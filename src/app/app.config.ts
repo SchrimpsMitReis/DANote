22f0da639ecc671610bc9e369216cb24b9776ff1
SchrimpsMitReis <roman.schroeder@inclufilm.com>
2024-06-23 04:02:47 +0200

    Clip 8

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,27 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 
 import { routes } from './app.routes';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    // provideFirebaseApp(() => initializeApp({ "projectId": "DANotes" })),
+    // provideFirestore(() => getFirestore()),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({
+      "projectId":"danote-36550",
+      "appId":"1:56153945629:web:e341ed8f83238f4954f8f4",
+      "storageBucket":"danote-36550.appspot.com",
+      "apiKey":"AIzaSyBysxr7Od1kefG8Pjit_6XiB4e6TFRUGAU",
+      "authDomain":"danote-36550.firebaseapp.com",
+      "messagingSenderId":"56153945629"
+    }))),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideFunctions(() => getFunctions())),
+  ]
+
 };
--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, collectionData, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = []
+
+  items$;
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    this.items$ = collectionData(this.getNotesRef());
+    
+  }
+  getNotesRef(){
+    return collection(this.firestore, 'notes');
+  }
+  getTrashRef(){
+    return collection(this.firestore, 'trash');
+  }
+  getSingleDocRef(colId:string, docId:string){
+    return doc(collection(this.firestore, colId),docId)
+  }
 }
