1ee89cc7d6495a46e7974aa5c97ad10c31a8647b
SchrimpsMitReis <roman.schroeder@inclufilm.com>
2024-06-24 11:10:54 +0200

    Video 13 Update

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { Note } from '../interfaces/note.interface';
-import { Firestore, collectionData, collection, doc, onSnapshot, addDoc } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, doc, onSnapshot, addDoc, updateDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -10,35 +10,30 @@ export class NoteListService {
   trashNotes: Note[] = [];
   normalNotes: Note[] = []
 
-  // items$;
-  // items;
   unsubNotes;
   unsubTrash;
-  // unsubList;
-  // unsubSingle;
+
   firestore: Firestore = inject(Firestore);
 
   constructor() {
     this.unsubNotes = this.subNotesList()
     this.unsubTrash = this.subTrashList()
-    // this.unsubSingle = onSnapshot(this.getSingleDocRef("nodes", "iFftYjpni0uj0nD5HF4q"), (element) =>{})
-    // this.items$ = collectionData(this.getNotesRef());
-    // this.items = this.items$.subscribe((list) =>{
-    //   list.forEach(element =>{
-    //     console.log(element)
-    //     // convToNode(element)
-    //   });
-
-    // })
   }
 
-  async addNote(item: {}){
+  async addNote(item: Note){
     await addDoc(this.getNotesRef(), item).catch( (e) =>{ console.error(e)})
     .then(
       (docRef) => {console.log("Document written with ID= ", docRef?.id);
       }
     )
   }
+
+  async updateNote(id: string, docId: string, item: {}){
+    await updateDoc(this.getSingleDocRef(id, docId), item).catch(
+      (e)=>{console.error(e)});
+    }
+
+
 
   ngOnDestroy(){
   this.unsubNotes()
